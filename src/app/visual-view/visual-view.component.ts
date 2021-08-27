aa7bab781c0960ced68971ce893a0036b177aded
imdaredevil <cibi_16@live.com>
2021-08-27 02:19:46 -0700

    pass data between parent and child

--- src/app/visual-view/visual-view.component.ts
+++ src/app/visual-view/visual-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Node } from '../data-objects';
 @Component({
   selector: 'app-visual-view',
   templateUrl: './visual-view.component.html',
   styleUrls: ['./visual-view.component.css']
 })
 export class VisualViewComponent implements OnInit {
-
   constructor() { }
 
+  @Input() nodeData: Node[] = []; 
+  
+  @Output() appendNode: EventEmitter<Node> = new EventEmitter<Node>();
+
   ngOnInit(): void {
+  }
+  createNewNode(): void {
+      this.appendNode.emit({
+          name: 'drag to create nodes',
+          x: window.innerWidth*0.65,
+          y: 10,
+          isCreator: true,
+      });
   }
 
 }
