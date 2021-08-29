4eeab5ae08aa4f9ae5ab0c7237da3a4d0f51a930
imdaredevil <cibi_16@live.com>
2021-08-29 01:06:49 -0700

    flask support

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__, static_folder='./dist/topology-generator')
+
+@app.route("/<name>/")
+def hello_world(name):
+    return redirect(url_for('static', filename='index.html'))
+
+@app.route("/")
+def hello():
+    return redirect(url_for('static', filename='index.html'))
